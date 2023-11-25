8efefbbe87c28eaec3ca583ee1aaafa630e3314f
syntaxerror <142847239+syntaxerror019@users.noreply.github.com>
2023-11-25 12:54:02 -0500

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,11 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hello, World!'
+    return render_template("index.html")
 
-@app.route('/about')
-def about():
-    return 'About'
+@app.route('/login')
+def login():
+    rurl = request.args.get('rurl')
+    return render_template("login.html")
+
+@app.route('/create')
+def create():
+    rurl = request.args.get('rurl')
+    return render_template("create.html")
